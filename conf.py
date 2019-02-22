c5654c6ba2244c8accdbcdf4aae6254a92f7e47e
Jc4Mn <guillaumecarcema@gmail.com>
2019-02-22 11:07:07 +0100

    function adding_data now loads database

--- conf.py
+++ conf.py
@@ -20,7 +20,11 @@ SITE = "https://world.openfoodfacts.org/cgi/search.pl?search_terms={}\
 &search_tag=category&page_size=1000&json=1"
 
 # list of categories
-CATEGORIES = ["'viande'", "'poisson'", "'pates'", "'legumes'", "'gateau'"]
+CATEGORIES = ["'viande'", "'poisson'", "'pates'", "'legumes'", "'gateau'",
+ "'conserve'", "'fromage'"]
 
 # insert categories request
 INSERT_REQ_CATEGORY = ("INSERT INTO Categories VALUES({}, {})")
+# insert products request
+INSERT_REQ_PRODUCTS = ("INSERT INTO Products VALUES {}")
+
--- creating_bdd.py
+++ creating_bdd.py
@@ -31,6 +31,7 @@ class BaseData (object):
                 cursor.execute('DROP DATABASE IF EXISTS comparatif_alimentaire;')
                 self.connection.commit()
             
+            
             # CREATING new database named comparatif_alimentaire
             with self.connection.cursor() as cursor:
                 cursor.execute("CREATE SCHEMA IF NOT EXISTS \
@@ -47,8 +48,8 @@ class BaseData (object):
             # CREATING table Products within comparatif_alimentaire
             with self.connection.cursor() as cursor:
                 cursor.execute("""CREATE TABLE IF NOT EXISTS `comparatif_alimentaire`.`Products` (
-                `id_products` INT UNSIGNED NOT NULL,
-                `product_name` VARCHAR(55) NOT NULL,
+                `id_products` BIGINT UNSIGNED NOT NULL,
+                `product_name` VARCHAR(150) NOT NULL,
                 `ingredients` TEXT(1900) NULL,
                 `url_ingredient` VARCHAR(250) NOT NULL,
                 `markets_for_product` VARCHAR(90) NULL,
@@ -68,7 +69,7 @@ class BaseData (object):
             with self.connection.cursor() as cursor:
                 cursor.execute("""CREATE TABLE IF NOT EXISTS `comparatif_alimentaire`.`favorites` (
                 `id_favorites` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                `id_link_product` INT UNSIGNED NOT NULL,
+                `id_link_product` BIGINT UNSIGNED NOT NULL,
                 PRIMARY KEY (`id_favorites`),
                 INDEX `fk_product_id_idx` (`id_link_product` ASC),
                 CONSTRAINT `fk_product_id`
@@ -77,6 +78,8 @@ class BaseData (object):
                   ON DELETE NO ACTION
                   ON UPDATE NO ACTION)
                 ENGINE = InnoDB;""")
+            
+            print("Creating database 'comparatif_alimentaire'")
 
         except:
             pass
@@ -85,30 +88,58 @@ class BaseData (object):
     def adding_data(self):
         "this function takes some data from a website and\
         puts it into database"
-            
+
+        #comparing products
+        compar=[]    
         # mark for products
-        t = 0
+        x= 0
         # each category will be implemented
         for x in range(len(CATEGORIES)):
             with self.connection.cursor() as cursor:
+                # adding data for Categories table
                 cursor.execute(INSERT_REQ_CATEGORY.format(x+1, CATEGORIES[x]))
                 self.connection.commit()
-#                   r=requests.get(SITE.format(categories[x])).json()
-#                   try:
-#                       data_product=[r["products"][t]["_id"], \
-#                       r["products"][t]["product_name_fr"], \
-#                       r["products"][t]["ingredients_text_debug"], \
-#                       r["products"][t]["url"], \
-#                       r["products"][t]["purchase_places"],
-#                       r["products"][t]["allergens"], \
-#                       r["products"][t]["nutrition_grades"]]
-#                       t+=1
-#                   except:
-#                       pass
-#
-        
-#test
-if __name__ == '__main__' :
-    bd= BaseData(USERNAME, PASSWORD)
+                
+                # getting data for Products table
+                print("Collecting data...")
+                r=requests.get(SITE.format(CATEGORIES[x])).json()
+                
+                #mark for each product
+                t, y = 0, 0
+                data_product = []
+                # creating loop to collect specific datas
+                while not y :
+                    try:
+                        if r["products"][t]["_id"] not in compar \
+                            and len(r["products"][t]["allergens"]) < 255:
+                            compar.append(r["products"][t]["_id"])
+                            data_product.append((r["products"][t]["_id"], 
+                            r["products"][t]["product_name_fr"], 
+                            r["products"][t]["ingredients_text_debug"], 
+                            r["products"][t]["url"], 
+                            r["products"][t]["purchase_places"],
+                            r["products"][t]["allergens"], 
+                            r["products"][t]["nutrition_grades"], 
+                            x+1))
+                            t+=1
+                            
+                        else:
+                            t+=1
+                            continue
+                    except:
+                        if t == 1000:
+                            y=1
+                            break
+                        else:
+                            t+=1
+    
+                #putting collected data in database
+                print("Data loading... pass level {}".format(x+1))
+                cursor.execute(INSERT_REQ_PRODUCTS.format(','.join(str(v) for v in data_product))+";")
+                self.connection.commit()
+      
+# test
+if __name__ == '__main__':
+    bd = BaseData(USERNAME, PASSWORD)
     bd.create_database()
     bd.adding_data()
